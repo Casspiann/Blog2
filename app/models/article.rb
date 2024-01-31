5ad0d642e27215eaaed4f7d0629e3874186d3241
TARUN <tarunrana4221@gmail.com>
2024-01-31 16:48:42 +0530

    adding Active user and User Authontication

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,12 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  def self.ransackable_associations(auth_object = nil)
+    ["comments"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["body", "created_at", "id", "id_value", "status", "title", "updated_at"]
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ApplicationRecord
   include Visible
 
   belongs_to :article
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["article_id", "body", "commenter", "created_at", "id", "id_value", "status", "updated_at"]
+  end
+  def self.ransackable_associations(auth_object = nil)
+    ["article"]
+  end
 end
